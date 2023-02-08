2c61ed8349e11447dca696dcb7e5581b7d4d07dd
olayasturias <olaya_93@hotmail.es>
2023-02-08 21:00:19 +0100

    adding gt and orb recorder

--- vo_all_at_once/scripts/read_save_pose.py
+++ vo_all_at_once/scripts/read_save_pose.py
@@ -1,13 +1,15 @@
 import rospy
 from geometry_msgs.msg import PoseStamped, TransformStamped
+from nav_msgs.msg import Odometry
 
 
 def save_groundtruth():
   print ("saving groundtruth")
 
-
-def gt_callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "Tartan cam pose %s", data.pose)
+def gt_PS_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "pose %s", data.pose)
+def gt_Odom_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "pose %s", data.pose)
     
 def listener():
 
@@ -15,8 +17,13 @@ def listener():
     rospy.on_shutdown(save_groundtruth)
 
     gt_topic = rospy.get_param('~pose_topic')
-
-    rospy.Subscriber(gt_topic, PoseStamped, gt_callback)
+    msg_type = rospy.get_param('~msg_type')
+    if msg_type == 'PoseStamped':
+        rospy.Subscriber(gt_topic, PoseStamped, gt_PS_callback)
+    # elif msg_type == 'TransformStamped':
+    #     rospy.Subscriber(gt_topic, TransformStamped, gt_callback)
+    elif msg_type == "Odometry":
+        rospy.Subscriber(gt_topic, Odometry, gt_Odom_callback)
     
     rospy.spin()
 
