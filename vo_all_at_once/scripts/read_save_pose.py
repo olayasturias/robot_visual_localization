acdc8ed90f8a5511ac7e453d3ffc535c5c45253a
olayasturias <olaya_93@hotmail.es>
2023-02-08 20:30:50 +0100

    first recorder attempt

---
+++ vo_all_at_once/scripts/read_save_pose.py
@@ -0,0 +1,22 @@
+import rospy
+from geometry_msgs.msg import PoseStamped, TransformStamped
+
+
+def save_groundtruth():
+  print ("saving groundtruth")
+
+
+def gt_callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    
+def listener():
+
+    rospy.init_node('gt_listener', anonymous=True)
+    gt_topic = rospy.get_param('~gt_topic')
+    rospy.Subscriber(gt_topic, PoseStamped, gt_callback)
+
+    rospy.on_shutdown(save_groundtruth)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
