ff6101a6a425a0404068b8ea6e054297fc62f472
olayasturias <olaya_93@hotmail.es>
2023-02-09 17:29:23 +0100

    record all and run all at the same time

--- vo_all_at_once/scripts/read_save_all_poses.py
+++ vo_all_at_once/scripts/read_save_all_poses.py
@@ -23,46 +23,52 @@ class Pose_msg_to_file():
         tartan_pose_topic = rospy.get_param('~tartan_pose_topic')
         tartan_msg_type = rospy.get_param('~tartan_msg_type')
 
-        self.gt_ts = [], self.gt_tx = [], self.gt_ty = [], self.gt_tz = []
-        self.gt_qx = [], self.gt_qy = [], self.gt_qz = [], self.gt_qw = []
+        self.gt_ts, self.gt_tx, self.gt_ty, self.gt_tz = [],[],[],[]
+        self.gt_qx, self.gt_qy, self.gt_qz, self.gt_qw = [],[],[],[]
 
-        self.orb_ts = [], self.orb_tx = [], self.orb_ty = [], self.orb_tz = []
-        self.orb_qx = [], self.orb_qy = [], self.orb_qz = [], self.orb_qw = []
+        self.orb_ts, self.orb_tx, self.orb_ty, self.orb_tz =  [],[],[],[]
+        self.orb_qx, self.orb_qy, self.orb_qz, self.orb_qw =  [],[],[],[]
 
-        self.tartan_ts = [], self.tartan_tx = [], self.tartan_ty = [], self.tartan_tz = []
-        self.tartan_qx = [], self.tartan_qy = [], self.tartan_qz = [], self.tartan_qw = []
+        self.tartan_ts, self.tartan_tx, self.tartan_ty, self.tartan_tz =  [],[],[],[]
+        self.tartan_qx, self.tartan_qy, self.tartan_qz, self.tartan_qw =  [],[],[],[]
 
         if gt_msg_type == 'PoseStamped':
-            rospy.Subscriber(gt_pose_topic, PoseStamped, self.gt_callback)
+            rospy.Subscriber(gt_pose_topic, PoseStamped, self.gt_callback, gt_msg_type)
         elif gt_msg_type == "Odometry":
-            rospy.Subscriber(gt_pose_topic, Odometry, self.gt_callback)
+            rospy.Subscriber(gt_pose_topic, Odometry, self.gt_callback, gt_msg_type)
 
         if orb_msg_type == 'PoseStamped':
-            rospy.Subscriber(orb_pose_topic, PoseStamped, self.orb_callback)
+            rospy.Subscriber(orb_pose_topic, PoseStamped, self.orb_callback, orb_msg_type)
         elif orb_msg_type == "Odometry":
-            rospy.Subscriber(orb_pose_topic, Odometry, self.orb_callback)
+            rospy.Subscriber(orb_pose_topic, Odometry, self.orb_callback, orb_msg_type)
         
         if tartan_msg_type == 'PoseStamped':
-            rospy.Subscriber(tartan_pose_topic, PoseStamped, self.tartan_callback)
+            rospy.Subscriber(tartan_pose_topic, PoseStamped, self.tartan_callback, tartan_msg_type)
         elif tartan_msg_type == "Odometry":
-            rospy.Subscriber(tartan_pose_topic, Odometry, self.tartan_callback)
+            rospy.Subscriber(tartan_pose_topic, Odometry, self.tartan_callback, tartan_msg_type)
 
-    def gt_callback(self,data): 
+    def gt_callback(self,data,msgtype): 
         self.read_pose(data, self.gt_ts, self.gt_tx, self.gt_ty, self.gt_tz,
-                             self.gt_qx, self.gt_qy, self.gt_qz, self.gt_qw)
-    def orb_callback(self,data): 
+                             self.gt_qx, self.gt_qy, self.gt_qz, self.gt_qw, msgtype)
+    def orb_callback(self,data,msgtype): 
         self.read_pose(data, self.orb_ts, self.orb_tx, self.orb_ty, self.orb_tz,
-                             self.orb_qx, self.orb_qy, self.orb_qz, self.orb_qw)
-    def tartan_callback(self,data): 
+                             self.orb_qx, self.orb_qy, self.orb_qz, self.orb_qw, msgtype)
+    def tartan_callback(self,data,msgtype): 
         self.read_pose(data, self.tartan_ts, self.tartan_tx, self.tartan_ty, self.tartan_tz,
-                             self.tartan_qx, self.tartan_qy, self.tartan_qz, self.tartan_qw)
+                             self.tartan_qx, self.tartan_qy, self.tartan_qz, self.tartan_qw, msgtype)
 
     def read_pose(self,data, ts_list, tx_list, ty_list, tz_list, 
-                        qx_list, qy_list, qz_list, qw_list):
-        timestamp = data.header.stamp.nsec
-        position_msg = data.pose.position
-        x, y, z = position_msg.x, position_msg.y, position_msg.z
-        orientation_msg = data.pose.orientation
+                        qx_list, qy_list, qz_list, qw_list, msgtype):
+        timestamp = data.header.stamp.nsecs
+
+        if msgtype  == 'PoseStamped':
+            position_msg = data.pose.position
+            orientation_msg = data.pose.orientation
+        elif msgtype == "Odometry":
+            position_msg = data.pose.pose.position
+            orientation_msg = data.pose.pose.orientation
+
+        x, y, z = position_msg.x, position_msg.y, position_msg.z        
         qx, qy, qz, qw = orientation_msg.x, orientation_msg.y, orientation_msg.z, orientation_msg.w
 
         ts_list.append(timestamp)
@@ -85,25 +91,35 @@ class Pose_msg_to_file():
         check_opts_rpe = {"align_origin": True,"align": False, "correct_scale": True, "all_pairs": True, "show_plot": True}
         
         result_dict = {}
+        trajectory_dict = {}
+        orb_aligned = []
+        tartan_aligned = []
 
         evo_gt     = file_interface.read_tum_trajectory_file(groundtruth)
-        evo_orb    = file_interface.read_tum_trajectory_file(orb)
-        evo_tartan = file_interface.read_tum_trajectory_file(tartan)
+        try:
+            evo_orb    = file_interface.read_tum_trajectory_file(orb)
+            r_dict, orb_aligned = self.create_evo_dict('orb_slam3', evo_gt, evo_orb)
+            result_dict.update(r_dict)
+            trajectory_dict.update({"orb_slam3": orb_aligned})
+        except:
+            print('no trajectory for ORB-SLAM3')
+        try:
+            evo_tartan = file_interface.read_tum_trajectory_file(tartan)
+            r_dict, tartan_aligned = self.create_evo_dict('tartan', evo_gt, evo_tartan)
+            result_dict.update(r_dict)
+            trajectory_dict.update({"tartan": tartan_aligned})
+        except:
+            print('no trajectory for TARTANVO')
 
-        r_dict, orb_aligned = self.create_evo_dict('orb_slam3', evo_gt, evo_orb)
-        result_dict.update(r_dict)
-        r_dict, tartan_aligned = self.create_evo_dict('tartan', evo_gt, evo_tartan)
-        result_dict.update(r_dict)
+        trajectory_dict.update({"reference": evo_gt})
 
-        print(result_dict)
-        with open(outfile + '.txt','w+') as data: 
-            data.write(str(result_dict))
-
-        trajectory_dict = {
-            "orb_slam3": orb_aligned,
-            "reference": evo_gt,
-            "tartan": tartan_aligned
-        }
+        f = open(outfile + '.txt', "w+")
+        f.write("{\n")
+        for key, nested_0 in result_dict.items():
+            for subkey, nested_1 in nested_0.items():
+                f.write("'{}':'{}':'{}'\n".format(key, subkey, nested_1))
+        f.write("}")
+        f.close()
 
         self.create_evo_plot(trajectory_dict, outfile+'.svg')
 
@@ -121,26 +137,26 @@ class Pose_msg_to_file():
         # APE translation metrics for orb
         pose_relation = metrics.PoseRelation.translation_part
         ape_metric = metrics.APE(pose_relation)
-        ape_metric.process_data(gt, estimate)
+        ape_metric.process_data((gt, estimate))
         ape_stats = ape_metric.get_all_statistics()
         result_dict.update({(algorithm_name,'ape','translation'):ape_stats})
         # APE rotation metrics for orb
         pose_relation = metrics.PoseRelation.rotation_angle_rad
         ape_metric = metrics.APE(pose_relation)
-        ape_metric.process_data(gt, estimate)
+        ape_metric.process_data((gt, estimate))
         ape_stats = ape_metric.get_all_statistics()
         result_dict.update({(algorithm_name,'ape','rotation'):ape_stats})
 
         # RPE translation metrics for orb
         pose_relation = metrics.PoseRelation.translation_part
         rpe_metric = metrics.RPE(pose_relation)
-        rpe_metric.process_data(gt, estimate)
+        rpe_metric.process_data((gt, estimate))
         rpe_stats = rpe_metric.get_all_statistics()
         result_dict.update({(algorithm_name,'rpe','translation'):rpe_stats})
         # RPE rotation metrics for orb
         pose_relation = metrics.PoseRelation.rotation_angle_rad
         rpe_metric = metrics.RPE(pose_relation)
-        rpe_metric.process_data(gt, estimate)
+        rpe_metric.process_data((gt, estimate))
         rpe_stats = rpe_metric.get_all_statistics()
         result_dict.update({(algorithm_name,'rpe','rotation'):rpe_stats})
 
